d28a72a5c849e472d6f5d4b2a8977f86156d19cc
Kevin Tai <tai@astron.nl>
2025-08-07 14:33:41 +0200

    Main changes

---
+++ sdc-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+import tailwindConfig from "@astron-sdc/design-system/tailwind.config.js";
+
+/** @type {import("tailwindcss").Config} */
+
+export default {
+  ...tailwindConfig,
+  // your overrides
+};
